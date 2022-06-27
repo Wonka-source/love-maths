f21f24ca3d4f2b21ed2a749fac53b803131747bb
Wonka-source <dan.eff117@gmail.com>
2022-06-27 21:37:15 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
 console.log("Connected")
+
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incorementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaysubtractQuestion() {
+
+}
+
+function displayMu1tiplyQuestion() {
+
+}
+
