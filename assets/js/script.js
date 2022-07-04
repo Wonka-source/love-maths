380906deaccebb45714063443d2cd17db44c40f5
Wonka-source <dan.eff117@gmail.com>
2022-07-04 20:55:44 +0000

    add division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
             checkAnswer();
         }
     })
-    runGame("addition", "multiply", "subtraction");
+    runGame("addition", "multiply", "subtraction", "division");
 })
 /**	
  * The main game "loop", called when the script is first loaded	
@@ -39,6 +39,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
         } else if (gameType === "subtract") {
             displaySubtractQuestion(num1, num2);
+        } else if (gameType === "division") {
+            displayDivisionQuestion(num1, num2);
         }else {	
         alert(`Unknown game type: ${gameType}`);	
         throw `Unknown game type: ${gameType}. Aborting!`;	
@@ -75,7 +77,9 @@ function calculateCorrectAnswer() {
             return[operand1 * operand2, "multiply"];
         } else if (operator === "-"){
             return[operand1 - operand2, "subtract"];
-        } else {
+        } else if (operator === "/"){
+            return[operand1 / operand2, "division"];
+        }else {
             alert(`Unimplemented operator ${operator}`);
             throw `Unimplemented operator ${operator}. Aborting!`;
         }
@@ -127,4 +131,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent =  operand1 * operand2;    
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
